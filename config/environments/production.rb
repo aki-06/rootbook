4f695f491c22be53580c68216d5697831a434a59
akihironemoto <a.nemoto@conol.jp>
2017-03-09 18:29:17 +0800

    heroku メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'warm-retreat-37455.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'warm-retreat-37455' }
 
 end
