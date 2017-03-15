60386081d8150758df8f7add1dff4a021e88dc04
akihironemoto <a.nemoto@conol.jp>
2017-03-15 22:57:38 +0900

    メッセージ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'relationships/create'
-
-  get 'relationships/destroy'
+  root 'top#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  root 'top#index'
 
   resources :users, only:[:index]
   resources :relationships, only:[:create, :destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  resources :conversations do
+    resources :messages
   end
 
   resources :topics do
