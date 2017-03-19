7a3d4a7c8240788554822005bac5866f4a00ccfc
akihironemoto <a.nemoto@conol.jp>
2017-03-19 13:53:36 +0900

    お知らせのtrue:falseを既読:未読に変更

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,7 +3,4 @@ module NotificationsHelper
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
 
-  def display_read_status(read)
-    read ? "既読" : "未読"
-  end
 end
