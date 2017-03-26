57fcf42b08d2598b6300bb487c4d2c7e55679a91
akihironemoto <a.nemoto@conol.jp>
2017-03-26 15:28:03 +0900

    メール送信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'warm-retreat-37455.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: '54.92.21.164' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
+    domain: "54.92.21.164",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'top#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
