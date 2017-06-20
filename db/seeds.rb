c662d9ca5234cd889525abaf5b5a52a4308c9194
akihironemoto <a.nemoto@conol.jp>
2017-06-20 14:26:30 +0900

    AWS上にTopicのシードデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,35 +11,35 @@
 # end
 
 #ユーザー
-20.times do
-  email = Faker::Internet.email
-  name = Faker::Name.name
-  password = "password"
-  num = format('%03d', [*001..141].sample)
-  image_url = "http://towncat.yokochou.com/images/#{num}.jpg"
-  user = User.new(
-          email: email,
-          name: name,
-          password: password,
-          password_confirmation: password,
-          uid: SecureRandom.uuid,
-          image_url: image_url
-        )
-  user.skip_confirmation!
-  user.save
-end
+# 20.times do
+#   email = Faker::Internet.email
+#   name = Faker::Name.name
+#   password = "password"
+#   num = format('%03d', [*001..141].sample)
+#   image_url = "http://towncat.yokochou.com/images/#{num}.jpg"
+#   user = User.new(
+#           email: email,
+#           name: name,
+#           password: password,
+#           password_confirmation: password,
+#           uid: SecureRandom.uuid,
+#           image_url: image_url
+#         )
+#   user.skip_confirmation!
+#   user.save
+# end
 
 #Topic
-# 20.times do
-#   user_id = [*1..20].sample
-#   title = Yoshida::Text.sentences(4).join
-#   content = Yoshida::Text.sentences(4).join
-#   num = format('%03d', [*001..141].sample)
-#   image = "http://towncat.yokochou.com/images/#{num}.jpg"
-#   Topic.create!(
-#     user_id: user_id,
-#     title: title,
-#     content: content,
-#     image: image
-#   )
-# end
+20.times do
+  user_id = [*1..20].sample
+  title = Yoshida::Text.sentences(4).join
+  content = Yoshida::Text.sentences(4).join
+  num = format('%03d', [*001..141].sample)
+  image = "http://towncat.yokochou.com/images/#{num}.jpg"
+  Topic.create!(
+    user_id: user_id,
+    title: title,
+    content: content,
+    image: image
+  )
+end
